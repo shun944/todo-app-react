d4949cefa2d441b2bbd6aeeb8a1370a382529c38
山本駿 <yamamotoshun@yamamotoshunnoMacBook-Pro.local>
2024-12-16 00:31:52 +0900

    refactor form for create and update todo

--- src/components/CreateTodoDialog/CreateTodoDialog.tsx
+++ src/components/CreateTodoDialog/CreateTodoDialog.tsx
@@ -3,22 +3,22 @@ import './CreateTodoDialog.css';
 import { CreateTodoRequest } from '../../models/Todo';
 import { UpdateTodoRequest } from '../../models/Todo';
 import { Todo } from '../../models/Todo';
-import dayjs, { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
 import { useAtom } from 'jotai';
 import { updatedFromDialogAtom, createdFromDialogAtom } from '../../atomJotai';
 import useCategories from '../../hooks/useCategories';
 
-import Textarea from '@mui/joy/Textarea';
-import { Button, Box } from '@mui/material';
+import { Button, Box, TextField, FormHelperText } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';     
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-
-
+import Select from '@mui/material/Select';
+import { Controller, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 interface CreateTodoDialogProps {
   onClose: () => void;
@@ -29,30 +29,26 @@ interface CreateTodoDialogProps {
   dialogOpen?: boolean;
 }
 
-interface TodoErrors {
-  title: boolean;
-  description: boolean;
-  start_date: boolean;
-  due_date: boolean;
-  category: boolean;
-}
-
 interface Category {
   id: number;
   name: string;
 }
 
+const schema = yup.object().shape({
+  title: yup.string().required('Title is required'),
+  description: yup.string().required('Description is required'),
+  start_date: yup.string().required('Start date is required'),
+  due_date: yup.string().required('Due date is required'),
+  category: yup.number().required('Category is required'),
+});
+
 const CreateTodoDialog: React.FC<CreateTodoDialogProps> = ({ onClose, onCreate, isUpdate, existingTodo, onUpdate, dialogOpen }) => {
-  const [todo, setTodo] = useState<CreateTodoRequest>({title: '', description: '', start_date: '', due_date: '', user_id: 0, category_master_id: 0});
-  const [updateTodo, setUpdateTodo] = useState<UpdateTodoRequest>({id: 0});
-  const [errors, setErrors] = useState<TodoErrors>({title: false, description: false, start_date: false, due_date: false, category: false});
-  const [errorMessages, setErrorMessages] = useState<string[]>([]);
-  const [startDateValue, setStartDateValue] = useState<Dayjs | null>(null);
-  const [dueDateValue, setDueDateValue] = useState<Dayjs | null>(null);
-  const [enableValidation, setEnableValidation] = useState(false);
-  const [selectedCategory, setSelectedCategory] = useState<string>('');
   const [updatedFromDialog, setUpdatedFromDialog] = useAtom(updatedFromDialogAtom);
   const [createdFromDialog, setCreatedFromDialog] = useAtom(createdFromDialogAtom);
+
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const { categories } = useCategories();
   const [categoryOptions, setCategoryOptions] = useState<Category[]>([]);
@@ -76,249 +72,135 @@ const CreateTodoDialog: React.FC<CreateTodoDialogProps> = ({ onClose, onCreate, 
     }
   }, [categories]);
 
-  useEffect(() => {
-    if(isUpdate && existingTodo) {
-      const previousTodo: UpdateTodoRequest = {
-        ...existingTodo,
-      };
-      setUpdateTodo(previousTodo);
-      setStartDateValue(dayjs(previousTodo.start_date));
-      setDueDateValue(dayjs(previousTodo.due_date));
-      setSelectedCategory(previousTodo.category_master_id?.toString() || '');
-    }
-    
-  }, [isUpdate, existingTodo, dialogOpen]);
-
-  useEffect(() => {
-    if (isUpdate) {
-      setStartDateValue(dayjs(updateTodo.start_date));
-      setDueDateValue(dayjs(updateTodo.due_date));
-      validationTodo(updateTodo, false);
-    } 
-    else {
-      setStartDateValue(dayjs(todo.start_date));
-      setDueDateValue(dayjs(todo.due_date));
-      if (!todo.due_date && !todo.start_date) {
-        setTodo({ ...todo, 
-          start_date: currentDate.format('YYYY-MM-DD'),
-          due_date: currentDate.format('YYYY-MM-DD')
-        });
-        
-      } else {
-        setTodo({ ...todo, 
-          start_date: todo.start_date,
-          due_date: todo.due_date
-        });
-      }
-      validationTodo(todo, false);
-    }
-  }, [updateTodo.start_date, todo.start_date, updateTodo.due_date, todo.due_date]);
-
-  useEffect(() => {
-    validationTodo(todo, false);
-    if (isUpdate) {
-      validationTodo(updateTodo, false);
-    } else {
-      validationTodo(todo, false);
-    }
-  }, [updateTodo.title, todo.title, updateTodo.description, todo.description]);
-  
-  const handleCreate = () => {
-    setEnableValidation(true);
-    const isValidTodo = validationTodo(todo, true);
-    if (!isValidTodo) {
-      return;
-    }
-    onCreate(todo);
+  const handleCreateClick = (data: any) => {  
+    const createRequest = setFormValueForCreate(data);
+    onCreate(createRequest);
     setCreatedFromDialog(true);
     onClose();
   };
 
-  const handleUpdate = () => {
-    setEnableValidation(true);
-    const isValidTodo = validationTodo(updateTodo, true);
-    if (!isValidTodo) {
-      return;
-    }
-    onUpdate(updateTodo);
+  const handleUpdateClick = (data: any) => {  
+    const updateRequest = setFormValueForUpdate(data);
+    onUpdate(updateRequest);
     setUpdatedFromDialog(true);
     onClose();
+  };
+
+  const setFormValueForCreate = (data: any) => {
+    const createTodoRequest: CreateTodoRequest = {
+      title: data.title,
+      description: data.description,
+      start_date: data.start_date,
+      due_date: data.due_date,
+      user_id: 0,
+      category_master_id: data.category
+    };
+    return createTodoRequest;
   }
 
-  const handleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    if (isUpdate) {
-      const tempTodo = {...updateTodo};
-      tempTodo.title = e.target.value;
-      setUpdateTodo(tempTodo);
-    } else {
-      setTodo({ ...todo, title: e.target.value });
+  const setFormValueForUpdate = (data: any) => {
+    const updateTodoRequest: UpdateTodoRequest = {
+      id: existingTodo?.id || 0,
+      title: data.title,
+      description: data.description,
+      start_date: data.start_date,
+      due_date: data.due_date,
+      category_master_id: data.category
     }
-
-    if (e.target.value) {
-      setErrors(prevErrors => ({...prevErrors, title: false}));
-      setErrorMessages(prevMessages => prevMessages.filter(message => message !== 'Title is required'));
-    } 
-  }
-
-  const handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    if (isUpdate) {
-      const tempTodo = {...updateTodo};
-      tempTodo.description = e.target.value;
-      setUpdateTodo(tempTodo);
-    } else {
-      setTodo({ ...todo, description: e.target.value });
-    }
-
-    if (e.target.value) {
-      setErrors(prevErrors => ({...prevErrors, description: false}));
-      setErrorMessages(prevMessages => prevMessages.filter(message => message !== 'Description is required'));
-    } 
-  }
-
-  const handleStartDateChange = (date: Dayjs | null) => {
-    let targetDate: string;
-    targetDate = date ? date.format('YYYY-MM-DD') : '';
-    if (isUpdate) {
-      const tempTodo = {...updateTodo};
-      tempTodo.start_date = targetDate;
-      setUpdateTodo(tempTodo);
-    } else {
-      setTodo({ ...todo, start_date: targetDate });
-    }
-
-    if (targetDate) {
-      setErrors(prevErrors => ({...prevErrors, start_date: false}));
-      setErrorMessages(prevMessages => prevMessages.filter(message => message !== 'Start date is required'));
-    }
-  }
-
-  const handleDueDateChange = (date: Dayjs | null) => {
-    let targetDate: string;
-    targetDate = date ? date.format('YYYY-MM-DD') : '';
-    if (isUpdate) {
-      const tempTodo = {...updateTodo};
-      tempTodo.due_date = targetDate;
-      setUpdateTodo(tempTodo);
-    } else {
-      setTodo({ ...todo, due_date: targetDate });
-    }
-
-    if (targetDate) {
-      setErrors(prevErrors => ({...prevErrors, due_date: false}));
-      setErrorMessages(prevMessages => prevMessages.filter(message => message !== 'Due date is required'));
-    } 
-  }
-
-  const handleSelectChange = (e: SelectChangeEvent) => {
-    setSelectedCategory(e.target.value);
-    if (isUpdate) {
-      const tempTodo = {...updateTodo};
-      tempTodo.category_master_id = Number(e.target.value);
-      setUpdateTodo(tempTodo);
-    } else {
-      setTodo({ ...todo, category_master_id: Number(e.target.value) });
-    }
-  }
-
-  const validationTodo = (todo: CreateTodoRequest | UpdateTodoRequest, isSubmited: boolean): boolean => {
-    if (!enableValidation && !isSubmited) {
-      return true;
-    }
-    let newErrors = {title: false, description: false, start_date: false, due_date: false, category: false};
-    let newErrorMessages: string[] = [];
-    if (!todo.title) {
-      newErrors = {...newErrors, title: true};
-      newErrorMessages = [...newErrorMessages, 'Title is required'];
-    }
-    if (!todo.description) {
-      newErrors = {...newErrors, description: true};
-      newErrorMessages = [...newErrorMessages, 'Description is required'];
-    }
-    if (!todo.start_date){
-      newErrors = {...newErrors, start_date: true};
-      newErrorMessages = [...newErrorMessages, 'Start date is required'];
-    }
-    if (!todo.due_date){
-      newErrors = {...newErrors, due_date: true};
-      newErrorMessages = [...newErrorMessages, 'Due date is required'];
-    }
-    if (dayjs(todo.start_date) > dayjs(todo.due_date)) {
-      newErrors = {...newErrors, start_date: true, due_date: true};
-      newErrorMessages = [...newErrorMessages, 'Start date must be before due date'];
-    }
-    if (todo.category_master_id === 0) {
-      newErrors = {...newErrors, category: true};
-      newErrorMessages = [...newErrorMessages, 'Category is required'];
-    }
-
-    setErrors(newErrors);
-    setErrorMessages(newErrorMessages);
-    setEnableValidation(false);
-
-    return !newErrorMessages.length;
+    return updateTodoRequest;
   }
 
   return (
     <div className='dialog'>
       <div className='dialog-content'>
-        <label htmlFor="title">title:</label>
-        <Textarea id="title" value={updateTodo.title} onChange={handleTitleChange}
-          sx={errors.title ? { borderColor: 'red' } : {}}
-        />
-        <label htmlFor="description">description:</label>
-        <Textarea id="description" value={updateTodo.description} onChange={handleDescriptionChange}
-          sx={{
-            height: '150px',
-            ...(errors.description ? { borderColor: 'red' } : {})}
-          }
-        />
-        <Box mt={2}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              label="start_date"
-              value={startDateValue || currentDate}
-              onChange={handleStartDateChange}
-              format='YYYY-MM-DD'
-              sx={{ marginBottom: 2, color: 'red' }}
-              
+        <form onSubmit={handleSubmit(isUpdate ? handleUpdateClick : handleCreateClick)}>
+          <Box sx={{ mt: 3, display: 'flex', flexDirection: 'column', gap: 1, mb: 3, width: '100%' }}>
+            <Controller name="title" control={control} 
+              defaultValue={existingTodo?.title} render={({ field }) => (
+                <TextField {...field} id="title" label="Title"
+                  type="title" size="small" error={!!errors.title}
+                  helperText={errors.title ? errors.title.message : ''}
+                />
+              )}
             />
-            <DatePicker
-              label="due_date"
-              value={dueDateValue}
-              onChange={handleDueDateChange}
-              format='YYYY-MM-DD'
+            <Controller name="description" control={control} 
+              defaultValue={existingTodo?.description} render={({ field }) => (
+                <TextField {...field} id="description" label="Description"
+                  type="description" multiline rows={4} error={!!errors.description}
+                  helperText={errors.description ? errors.description.message : ''}
+                />
+              )}
             />
-          </LocalizationProvider>
-        </Box>
-        <FormControl error={errors.category} sx={{ m: 1, minWidth: 120 }} size="small">
-          <InputLabel id="demo-select-small-label">Category</InputLabel>
-          <Select
-            labelId="demo-select-small-label"
-            id="demo-select-small"
-            value={selectedCategory}
-            label="Age"
-            onChange={handleSelectChange}
-          >
-            {categoryOptions.map((category) => (
-              <MenuItem key={category.id} value={category.id.toString()}>{category.name}</MenuItem>
-            ))}
-          </Select>
-      </FormControl>
-        <div>
-          <Box mt={2}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <Controller name="start_date" control={control}
+                defaultValue={existingTodo?.start_date ? existingTodo.start_date : currentDate.format('YYYY-MM-DD')}
+                render={({ field }) => (
+                  <DatePicker
+                    {...field}
+                    label="start_date" value={dayjs(field.value) || null} 
+                    format='YYYY-MM-DD'
+                    onChange={(newValue) => {
+                      field.onChange(newValue ? newValue.format('YYYY-MM-DD') : null);
+                    }}
+                    slotProps={{
+                      textField: {
+                        error: !!errors.start_date, helperText: errors.start_date ? errors.start_date.message : ''
+                      }
+                    }}
+                  />
+                )}
+              />
+              <Controller name="due_date" control={control}
+                defaultValue={existingTodo?.due_date ? existingTodo.due_date : currentDate.format('YYYY-MM-DD')}
+                render={({ field }) => (
+                  <DatePicker
+                    {...field}
+                    label="due_date" value={dayjs(field.value) || null} 
+                    format='YYYY-MM-DD'
+                    onChange={(newValue) => {
+                      field.onChange(newValue ? newValue.format('YYYY-MM-DD') : null);
+                    }}
+                    slotProps={{
+                      textField: {
+                        error: !!errors.start_date, helperText: errors.start_date ? errors.start_date.message : ''
+                      }
+                    }}
+                  />
+                )}
+              />
+            </LocalizationProvider>
+            <Controller name="category" control={control}
+              defaultValue={existingTodo?.category_master_id ? existingTodo.category_master_id : 1}
+              render={({ field }) => (
+                <FormControl fullWidth error={errors.category ? true : false}>
+                  <InputLabel id="select-label">Category</InputLabel>
+                  <Select
+                    labelId="select-label" id="select"
+                    label="Category" {...field}
+                    value={field.value || ''}
+                    onChange={(event) => {
+                      // 選択された値をフォームに更新
+                      field.onChange(event.target.value);
+                    }}
+                  >
+                    {categoryOptions.map((category) => (
+                      <MenuItem key={category.id} value={category.id.toString()}>{category.name}</MenuItem>
+                    ))}
+                  </Select>
+                  <FormHelperText>
+                    {errors.category?.message}
+                  </FormHelperText>
+                </FormControl>
+                )}
+            />
             {
               isUpdate  
-              ? <Button variant="contained" onClick={handleUpdate}>Update</Button>
-              : <Button variant="contained" onClick={handleCreate}>Create</Button>
+              ? <Button type="submit" variant="contained">Update</Button>
+              : <Button type="submit" variant="contained">Create</Button>
             }
-          </Box>
-          {errorMessages && errorMessages.map((message, index) => 
-            <p className="red" key={index}>{message}</p>)
-          }
-          <Box display="flex" justifyContent="flex-end">
             <Button variant="contained" onClick={onClose} color='inherit'>Close</Button>
           </Box>
-        </div>
+        </form>
       </div>
     </div>
   );
